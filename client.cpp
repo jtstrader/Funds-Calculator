29ff0fef10226a12a3c34c16d7083ee9a3f6e606
Jacob Strader <jacob.thomas.strader@live.mercer.edu>
2021-05-09 17:54:34 -0400

    added a main menu.

--- client.cpp
+++ client.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+char menu(Data& userData);
 bool checkArgs(char* dataFile);
 void getName(string& NEW_NAME);
 void printHelp();
@@ -11,7 +12,7 @@ int main(int argc, char** argv) {
     // no flags for this program
     string USER_NAME;
     if(argc!=2) {
-        cout<<"bank: Invalid syntax. Use bank --help for more information."<<endl;
+        cout<<"FundsCalculator: Invalid syntax. Use FundsCalculator --help for more information."<<endl;
         return 1;
     }
     else if(strcmp(argv[1], "--help") == 0) {
@@ -20,10 +21,10 @@ int main(int argc, char** argv) {
         return 0;
     }
     else if(!checkArgs(argv[1])) {
-        cout<<"bank: File "<<argv[1]<<" not found."<<endl;
+        cout<<"FundsCalculator: File "<<argv[1]<<" not found."<<endl;
         cout<<"Would you like to create a new file with this name? [Y/N]: ";
         string input; char letter;
-        cin>>input; letter = toupper(input[0]);
+        getline(cin, input); letter = toupper(input[0]);
         if(letter == 'Y') {
             fstream newFile(argv[1], ios::out | ios::binary); // create new file
             newFile.close();
@@ -32,8 +33,34 @@ int main(int argc, char** argv) {
     }
 
     // passed all error checks, open file and begin transaction tracking
+    Data* userData = new Data(argv[1], USER_NAME);
+    char option;
+    do {
+        option = menu(*userData);
+
+    } while(option!='e');
+
     
     return 0;
+}
+
+char menu(Data& userData) {
+    cout<<"---CURRENT ACCOUNT INFO---"<<endl;
+    userData.Write();
+    cout<<"--------------------------"<<endl<<endl;
+
+    cout<<"|======= OPTIONS ========|"<<endl;
+    cout<<"| a. Deposit Funds       |"<<endl;
+    cout<<"| b. Withdraw Funds      |"<<endl;
+    cout<<"| c. Remove Transaction  |"<<endl; 
+    cout<<"| d. Update Transaction  |"<<endl;
+    cout<<"| e. EXIT                |"<<endl;
+    cout<<"|========================|"<<endl<<endl;
+    cout<<"| Please select an option: ";
+    
+    string input; getline(cin, input);
+    char letter = tolower(input[0]);
+    return letter;
 }
 
 bool checkArgs(char* dataFile) {
@@ -62,7 +89,7 @@ void getName(string& NEW_NAME) {
             // valid name obtained, confirm with user.
             cout<<"Your name is: "<<NEW_NAME<<endl;
             cout<<"Keep this name? [Y/N]: ";
-            string check; cin>>check;
+            string check; getline(cin, check);
             if(toupper(check[0])=='Y')
                 ex = true; // break out of loop
         }
@@ -70,6 +97,6 @@ void getName(string& NEW_NAME) {
 }
 
 void printHelp() {
-    cout<<"usage: bank [DATA_FILE]"<<endl;
+    cout<<"usage: FundsCalculator [DATA_FILE]"<<endl;
     return;
 }
