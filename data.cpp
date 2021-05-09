dbdbfc6f9164cfc5abf3743227c64eaafac91880
Jacob Strader <jacob.thomas.strader@live.mercer.edu>
2021-05-09 19:35:40 -0400

    finalized << overalods

--- data.cpp
+++ data.cpp
@@ -1,6 +1,33 @@
 #include "data.h"
 
 ////////////////////////////////////////
+// FRIEND FUNCTIONS                   //
+////////////////////////////////////////
+ostream& operator << (ostream& stream, UserInfo& uinf) {
+    // USER INFO OVERLOADING
+    return (
+        stream<<"|=== "<<uinf.USER_NAME<<endl
+              <<"| Checking: $"<<fixed<<setprecision(2)<<uinf.CHECKING_BALANCE<<endl
+              <<"|  Savings: $"<<fixed<<setprecision(2)<<uinf.SAVINGS_BALANCE<<endl
+              <<"|    Total: $"<<fixed<<setprecision(2)<<uinf.TOTAL_BALANCE<<endl
+              <<"|=== "<<endl    
+    );
+}
+
+ostream& operator << (ostream& stream, Transaction& tinf) {
+    // TRANSACTION OVERLOADING
+    stream<<"|"
+          <<(tinf.a_type==CHECKING?"CHECKING":"SAVINGS")<<"|"
+          <<(tinf.t_type==ADD?"Deposit":"Withdrawal")<<"\t ";
+    
+    if(tinf.t_type==SUBTRACT)
+        stream<<"-";
+    
+    stream<<"$"<<tinf.change<<endl;
+    return stream;
+}
+
+////////////////////////////////////////
 // PUBLIC FUNCTIONS                   //
 ////////////////////////////////////////
 Data::Data(char* fileName, string USER_NAME) {
@@ -42,7 +69,7 @@ void Data::close() {
 // add transaction to the binary file.
 void Data::createNewTransaction(TransactionType t_type, AccountType a_type, float change) {
     // get last transaction record
-    long backId = 0;
+    long backId = -1;
     if(a_type == CHECKING && checkingRecords.size()>0) {
         map<long, Transaction>::iterator it = checkingRecords.end();
         it--; backId = it->first;
@@ -70,34 +97,10 @@ void Data::createNewTransaction(TransactionType t_type, AccountType a_type, floa
             uInfo.NUM_SAVINGS_TRANS++;
             break;
     }
-    
-    
-    
-    
-    
-    dataFile.seekg(0, ios::end); // go to end of file, add new record
-
-
-    
 }
 
-/*ostream& operator << (ostream& stream, UserInfo& uinf) {
-    return (
-        stream<<"|=== "<<uinf.USER_NAME<<endl
-              <<"| Checking: $"<<fixed<<setprecision(2)<<uinf.CHECKING_BALANCE<<endl
-              <<"|  Savings: $"<<fixed<<setprecision(2)<<uinf.SAVINGS_BALANCE<<endl
-              <<"|    Total: $"<<fixed<<setprecision(2)<<uinf.TOTAL_BALANCE<<endl
-              <<"|=== "<<endl
-              
-    );
-}*/
-
-void Data::Write() {
-    cout<<"|=== "<<uInfo.USER_NAME<<endl
-        <<"| Checking: $"<<fixed<<setprecision(2)<<uInfo.CHECKING_BALANCE<<endl
-        <<"|  Savings: $"<<fixed<<setprecision(2)<<uInfo.SAVINGS_BALANCE<<endl
-        <<"|    Total: $"<<fixed<<setprecision(2)<<uInfo.TOTAL_BALANCE<<endl
-        <<"|=== "<<endl;
+void Data::WriteUserInfo() {
+    cout<<uInfo;
 }
 
 ////////////////////////////////////////
@@ -106,17 +109,3 @@ void Data::Write() {
 ////////////////////////////////////////
 // PRIVATE FUNCTIONS                  //
 ////////////////////////////////////////
-
-bool Data::checkDecimal(string input) {
-
-    // Under the assumption that client file takes care of
-    // any errors involving invalid input. string input is
-    // for the sake of avoiding trailing zeroes obtained when
-    // using to_string()
-
-    size_t found = input.find('.');
-    
-    if(found!=string::npos && (input.length()-found>3))
-        return false; // input.length()-found > 3 implies more than 2 decimals
-    return true;
-}
